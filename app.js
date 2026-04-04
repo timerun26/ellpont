8f40dbb7bcbc32d3b0438365fb3406e9d72d84d0
timerun26 <sajtosali@gmail.com>
2026-04-04 23:33:16 +0200

    Update date handling in ujUgyfel function

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ function ujUgyfel() {
   let nev = document.getElementById("nev").value;
   let cim = document.getElementById("cim").value;
 
-  let now = Date.now();
+  let now = new Date(Date.now());
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, '0'); 
+  const day = String(d.getDate()).padStart(2, '0');
+  
+  const hours = String(d.getHours()).padStart(2, '0');
+  const minutes = String(d.getMinutes()).padStart(2, '0');
+  const seconds = String(d.getSeconds()).padStart(2, '0');
+
+let datum = ${year}-${month}-${day} ${hours}:${minutes}:${seconds};
+
 
   db.run(
     "INSERT INTO ugyfelek (nev, cim, modositva, sync) VALUES (?, ?, ?, 0)",
-    [nev, cim, now]
+    [nev, cim, datum]
   );
 
   saveDB();
@@ -22,7 +32,7 @@ function listaFrissit() {
   if (res.length > 0) {
     for (let row of res[0].values) {
       html += `<div>
-        <b>${row[1]}</b> – ${row[2]}
+        <b>${row[1]}</b> – ${row[3]}
       </div>`;
     }
   }
