1812a4de1f98df3c87349e4ff9ddbf587226df67
timerun26 <sajtosali@gmail.com>
2026-04-05 11:19:59 +0200

    Create dblog.js

---
+++ dblog.js
@@ -0,0 +1,31 @@
+let db;
+
+async function initDB() {
+  const SQL = await initSqlJs({
+    locateFile: file => 
+      `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/${file}`
+  });
+
+  let saved = localStorage.getItem("adatbazis");
+
+  if (saved) {
+    db = new SQL.Database(new Uint8Array(JSON.parse(saved)));
+  } else {
+    db = new SQL.Database();
+    db.run(`
+      CREATE TABLE logok (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+		hely TEXT,
+		adat TEXT,
+		idopont TEXT,
+		talalat TEXT
+      );
+    `);
+    saveDB();
+  }
+}
+
+function saveDB() {
+  const data = db.export();
+  localStorage.setItem("adatbazis", JSON.stringify(Array.from(data)));
+}
